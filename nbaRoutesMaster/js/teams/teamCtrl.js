b0f80faffcc0f426251734f32a660c64c7eff999
Phillip Puckett <phillippuckett88@gmail.com>
2015-11-05 13:45:03 -0700

    Changed Info display, and toggle feature

--- nbaRoutesMaster/js/teams/teamCtrl.js
+++ nbaRoutesMaster/js/teams/teamCtrl.js
@@ -139,21 +139,22 @@ app.controller('teamCtrl', function ($scope, $stateParams, teamData, teamService
     $scope.showNewGameForm = false;
     $scope.toggleNewGameForm = function () {
         $scope.showNewGameForm = !$scope.showNewGameForm;    
-        if ($stateParams.teams === /teams/utahjazz) {
+    } 
+            if ($stateParams.team === 'utahjazz') {
             $scope.homeTeam = 'Utah Jazz';
             $scope.logoPath = 'images/jazz-logo.png'
         }
-        else if ($stateParams.teams === /teams/miamiheaet) {
+        else if ($stateParams.team === 'miamiheat') {
             $scope.homeTeam = 'Miami Heat';
             $scope.logoPath = 'images/heat-logo.png'
         }
-        else if ($stateParams.teams === /teams/losangeleslakers) {
+        else if ($stateParams.team === 'losangeleslakers') {
             $scope.homeTeam = 'Los Angeles Lakers';
             $scope.logoPath = 'images/lakers-logo.png'
         } else {
             return '/';
         }
-    } 
+        console.log($scope.logoPath);
     $scope.submitGame = function () { 
         $scope.newGame.homeTeam = $scope.homeTeam.split(' ').join('').toLowerCase();     
     teamService.addNewGame($scope.newGame)
@@ -166,4 +167,5 @@ app.controller('teamCtrl', function ($scope, $stateParams, teamData, teamService
         });
     });
    };
+   console.log($scope);
 });
